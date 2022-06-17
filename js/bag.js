4c6d07d9159bbbe1ad463037be0fd840eddefb1d
asheesh jaat <asheeshjaat2175@gmail.com>
2022-06-17 16:35:17 +0530

    update payment

--- js/bag.js
+++ js/bag.js
@@ -17,22 +17,22 @@ function showmore() {
 
 
   const append = document.querySelector('#append');
-  let data=[ {
-    "id": "1",
-    "Image": "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2022/3/2/33f426d3-752f-45fa-8344-e0b16f48bfc61646215571249-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Cotton-Pure-Cotton--7.jpg~https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2022/3/2/1d0092fa-f331-4713-9ac9-719c9032618b1646215571270-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Cotton-Pure-Cotton--5.jpg~https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2022/3/2/14eab570-0d13-4b75-b943-4ceb207b540d1646215571260-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Cotton-Pure-Cotton--6.jpg~https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2022/3/2/b29281a6-99d0-4ed2-a54e-1b239553f7471646215571297-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Cotton-Pure-Cotton--2.jpg~https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2022/3/2/093bc645-d461-4128-94a1-0692803944141646215571321-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Cotton-Pure-Cotton--1.jpg",
-    "Name": "HRX by Hrithik Roshan",
-    "Price": "314",
-    "Discripation": "Keep this hip this season with the HRX Men's Athleisure T-shirt. This versatile T-shirt can be styled any way you like for the ultimate gym-to-street look.",
-    "rating": "4",
-    "category": "t-sirts",
-    "discounts":"40",
-  },];
+  // let data=[ {
+  //   "id": "1",
+  //   "Image": "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2022/3/2/33f426d3-752f-45fa-8344-e0b16f48bfc61646215571249-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Cotton-Pure-Cotton--7.jpg~https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2022/3/2/1d0092fa-f331-4713-9ac9-719c9032618b1646215571270-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Cotton-Pure-Cotton--5.jpg~https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2022/3/2/14eab570-0d13-4b75-b943-4ceb207b540d1646215571260-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Cotton-Pure-Cotton--6.jpg~https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2022/3/2/b29281a6-99d0-4ed2-a54e-1b239553f7471646215571297-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Cotton-Pure-Cotton--2.jpg~https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/1700944/2022/3/2/093bc645-d461-4128-94a1-0692803944141646215571321-HRX-by-Hrithik-Roshan-Men-Yellow-Printed-Cotton-Pure-Cotton--1.jpg",
+  //   "Name": "HRX by Hrithik Roshan",
+  //   "Price": "314",
+  //   "Discripation": "Keep this hip this season with the HRX Men's Athleisure T-shirt. This versatile T-shirt can be styled any way you like for the ultimate gym-to-street look.",
+  //   "rating": "4",
+  //   "category": "t-sirts",
+  //   "discounts":"40",
+  // },];
  
-let data1=JSON.parse(localStorage.getItem("cart"));
+let data=JSON.parse(localStorage.getItem("cart"));
 
 let Mrp=0;
 let totalprice=0;
-
+  
 
 
   
